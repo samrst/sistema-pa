fd1a80798592b81384a06b8fd34035889f1bc213
Sam Rodrigues <samrstt12@gmail.com>
2026-08-11 11:00:17 -0300

    Add 'Geral' to CAPACIDADES array

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -29,7 +29,7 @@ export const CURSOS = [
 ] as const;
 
 export const CAPACIDADES = [
-  "Capacidades Socioemocionais", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10","Outras",
+  "Geral", "Capacidades Socioemocionais", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10","Outras",
 ] as const;
 
 export const MODALIDADE = [
